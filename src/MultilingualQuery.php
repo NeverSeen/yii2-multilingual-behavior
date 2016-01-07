0b86a1d192f9f07d38082a64472180b96b8d5b3e
Ignas Strakšas <neverseenone@gmail.com>
2016-01-07 12:50:55 +0200

    Update MultilingualQuery.php

--- src/MultilingualQuery.php
+++ src/MultilingualQuery.php
@@ -6,4 +6,16 @@ use yii\db\ActiveQuery;
 class MultilingualQuery extends ActiveQuery
 {
     use MultilingualTrait;
+    
+    public function count($q = '*', $db = null) {
+        
+        $this->joinWith('translation')->andWhere(['languages_id' => Languages::getId()]);
+        return parent::count($q, $db);
+    }
+    
+    public function all($db = null){
+        
+        $this->joinWith('translation');
+        return parent::all($db);
+    }
 }
